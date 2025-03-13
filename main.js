b9c5bf52fd64f60d42eb415e571da4ef8c9dd1fc
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-13 17:37:02 +0100

    feat: add email list

--- main.js
+++ main.js
@@ -3,11 +3,29 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
 axios.get(endpoint)
     .then((response) => {
+        console.log(response)
 
         const emails = response.data;
         console.log(emails);
+        
+        const email = emails.response;
+        console.log(email);
 
-        console.log(emails.response);
+        const ulElement = document.querySelector('.list');
+
+        console.log(ulElement)
+
+        let emailList = '';
+
+        for (let i = 0; i < 10; i++) {
+                
+            emailList += `<li>${email}</li>`
+        
+        }
+
+        ulElement.innerHTML = emailList;
+        
+        
 
     })
     .catch(error => {
